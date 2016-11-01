69bdf2eac484a366604298ddf1844132bb4282e8
Ronny Tomasetti <ronny.tomasetti@gmail.com>
2016-11-01 10:56:16 -0400

    Test news scraping.

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+var request = require('request');
+var cheerio = require('cheerio');
+
 router.get('/', function(req, res, next) {
-	res.render('index', {
-		title: 'Express'
+
+	request('http://sdtimes.com/', function(error, response, html) {
+
+		var $ = cheerio.load(html);
+
+		$('col-md-12 latestnewslist').children().each(function(i, element) {
+
+			console.log('ELEMENT: ', i, ' ', element);
+
+			var title = $(element).children();
+
+			console.log("==================================");
+			console.log(title);
+			console.log("==================================");
+		});
+
 	});
+
+	// res.render('index', {
+	// 	title: 'Express'
+	// });
+
+	// res.send('respond with a resource');
 });
 
 module.exports = router;
