0c6a1a71a6310e624869d34268488a959819d842
Ronny Tomasetti <ronny.tomasetti@gmail.com>
2016-11-04 23:36:41 -0400

    Implement get home and scrape routes.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Mongo News
+
+## Overview
+----------
+News web app using Mongo and web scraping with Cheerio. The name of this application might be Mongo News but thats only so to keep you on your toes. There is no news on Mongo here. All of the articles are about Node from blog at https://nodesource.com, take that...   ಠ_ಠ
+
+----------
--- routes/index.js
+++ routes/index.js
@@ -4,30 +4,55 @@ var router = express.Router();
 var request = require('request');
 var cheerio = require('cheerio');
 
-router.get('/', function(req, res, next) {
+var Article = require('../models/Article');
+var Note = require('../models/Note');
 
-	request('http://sdtimes.com/', function(error, response, html) {
+/**
+ * GET '/' route renders Mongo News homepage
+ */
+router.get('/', function(req, res, next) {
+	res.render('index');
+});
+
+router.get('/scrape', function(req, res) {
+
+	request('https://nodesource.com/blog', function(error, response, html) {
 
 		var $ = cheerio.load(html);
 
-		$('col-md-12 latestnewslist').children().each(function(i, element) {
+		$('article h2').each(function(i, element) {
 
-			console.log('ELEMENT: ', i, ' ', element);
+			var result = {};
 
-			var title = $(element).children();
+			result.title = $(this).children('a').text();
+			result.link = "https://nodesource.com" + $(this).children('a').attr('href');
 
-			console.log("==================================");
-			console.log(title);
-			console.log("==================================");
+			var entry = new Article(result);
+
+			entry.save(function(err, doc) {
+				if (err)
+					console.log('Error ', err);
+				else
+					console.log('Save successful ', doc);
+			});
+
 		});
-
 	});
 
-	// res.render('index', {
-	// 	title: 'Express'
-	// });
+	res.status(200).json({
+		success: true,
+		message: 'Scrape completed succesfully.'
+	});
 
-	// res.send('respond with a resource');
+});
+
+router.get('/articles', function(req, res) {
+	Article.find({}, function(err, doc) {
+		if (err)
+			console.log(err);
+		else
+			res.json(doc);
+	});
 });
 
 module.exports = router;
